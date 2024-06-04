4f7312584e3d4fc618370762e62b646dd7b054d5
Mees-H <mhurenkamp@huurie.nl>
2024-06-04 20:37:21 +0200

    refactor complete!

--- Data/Session.fs
+++ Data/Session.fs
@@ -36,6 +36,10 @@ let sessionPersistance =
         member this.GetEligibleSessions (name: string) (shallowOk: bool) (minMinutes: int) = 
             InMemoryDatabase.all store.sessions
             |> Seq.filter (fun (n, _, _, _) -> n = name)
+            /// If shallowOk is false then filter out all sessions with deep = false
+            |> Seq.filter (fun (_, deep, _, _) -> shallowOk || deep)
+            /// Filter out all sessions with minutes < minMinutes
+            |> Seq.filter (fun (_, _, _, minutes) -> minutes >= minMinutes)
             |> Seq.map (fun (_, deep, date, minutes) ->
                 { Session.Deep = deep
                   Date = date
@@ -45,6 +49,10 @@ let sessionPersistance =
         member this.GetTotalEligibleMinutes (name: string) (shallowOk: bool) (minMinutes: int) = 
             InMemoryDatabase.all store.sessions
             |> Seq.filter (fun (n, _, _, _) -> n = name)
+            /// If shallowOk is false then filter out all sessions with deep = false
+            |> Seq.filter (fun (_, deep, _, _) -> shallowOk || deep)
+            /// Filter out all sessions with minutes < minMinutes
+            |> Seq.filter (fun (_, _, _, minutes) -> minutes >= minMinutes)
             |> Seq.map (fun (_, _, _, minutes) -> minutes)
             |> Seq.sum
     }
